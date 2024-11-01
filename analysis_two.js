d013abedb698bb75361ce22ce37ab67cd260e410
stanislavh <stanislav.honcharov@sonarsource.com>
2024-11-01 14:24:28 +0100

    trigger analysis 9.9

---
+++ analysis_two.js
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2009-2024 SonarSource SA
+ * All rights reserved
+ * mailto:info AT sonarsource DOT com
+ */
+
+const crypto = require("crypto");
+const key = "123";
+const iv = "12345";
+
+crypto.createCipheriv("DES", key, iv); // Noncompliant
+crypto.createCipheriv("DES", key, iv); // Noncompliant
+
+const password = "1237";
+console.log(password); // sensitive
+console.log(password); // sensitive
